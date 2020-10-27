94c18e01c19ea841da344c595c8d9958c90f3c96
Efe Hıdır <42965116+efehidir@users.noreply.github.com>
2020-10-27 20:28:24 +0300

    Update app.js
    Geliştirmeler yapıldı.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const fetch = require('node-fetch');
+module.exports = {
+	version: "1.0.9"
+}
+module.exports.gunluk = async(burc) => {
+	const fetch = require('node-fetch');
 const { JSDOM } = require('jsdom');
-
-let data = []
+	let data = []
 let silAr = []
 
-var gunluk = "Günlük Burç Yorumları"
-
-async function gunluk.oglak() {
-	let oglak = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/oglak-burcu/')
+if (burc.toLowerCase() === "oglak" || burc.toLowerCase() == "oğlak") {
+let oglak = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/oglak-burcu/')
 	let oglak1 = await oglak.text()
 	  let dom = new JSDOM(oglak1)
  let news = dom.window.document.querySelectorAll('p');
@@ -16,9 +17,9 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.yengec() {
-    let yengec = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/yengec-burcu/')
+} else
+if (burc.toLowerCase() === "yengec" || burc.toLowerCase() == "yengeç") {
+let yengec = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/yengec-burcu/')
 	let yengec1 = await yengec.text()
 	  let dom = new JSDOM(yengec1)
  let news = dom.window.document.querySelectorAll('p');
@@ -27,8 +28,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.koc() {
+} else 
+if (burc.toLowerCase() === "koc" || burc.toLowerCase() == "koç") {
 let koc = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/koc-burcu/')
 	let koc1 = await koc.text()
 	  let dom = new JSDOM(koc1)
@@ -38,8 +39,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.boga() {
+} else
+if (burc.toLowerCase() === "boga" || burc.toLowerCase() == "boğa") {
 let boga = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/boga-burcu/')
 	let boga1 = await boga.text()
 	  let dom = new JSDOM(boga1)
@@ -49,8 +50,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.ikizler() {
+} else
+if (burc.toLowerCase() === "ikizler") {
 let ikizler = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/ikizler-burcu/')
 	let ikizler1 = await ikizler.text()
 	  let dom = new JSDOM(ikizler1)
@@ -60,8 +61,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.aslan() {
+} else
+if (burc.toLowerCase() === "aslan") {
 let aslan = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/aslan-burcu/')
 	let aslan1 = await aslan.text()
 	  let dom = new JSDOM(aslan1)
@@ -72,7 +73,7 @@ news.forEach(a=>silAr.push(a.textContent))
 	   
     return data.join(",\n")
 }
-async function gunluk.basak() {
+if (burc.toLowerCase() === "basak" || burc.toLowerCase() == "başak") {
 let basak = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/basak-burcu/')
 	let basak1 = await basak.text()
 	  let dom = new JSDOM(basak1)
@@ -82,8 +83,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.terazi() {
+} else
+if (burc.toLowerCase() === "terazi") {
 let terazi = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/terazi-burcu/')
 	let terazi1 = await terazi.text()
 	  let dom = new JSDOM(oglak1)
@@ -93,8 +94,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.akrep() {
+} else
+if (burc.toLowerCase() === "akrep") {
 let akrep = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/akrep-burcu/')
 	let akrep1 = await akrep.text()
 	  let dom = new JSDOM(akrep1)
@@ -104,8 +105,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.yay() {
+} else
+if (burc.toLowerCase() === "yay") {
 let yay = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/yay-burcu/')
 	let yay1 = await yay.text()
 	  let dom = new JSDOM(yay1)
@@ -115,8 +116,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.kova() {
+} else
+if (burc.toLowerCase() === "kova") {
 let kova = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/kova-burcu/')
 	let kova1 = await kova.text()
 	  let dom = new JSDOM(kova1)
@@ -126,8 +127,8 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
-}
-async function gunluk.balik() {
+} else
+if (burc.toLowerCase() === "balik" || burc == "balık") {
 let balik = await fetch('https://www.hurriyet.com.tr/mahmure/astroloji/balik-burcu/')
 	let balik1 = await balik.text()
 	  let dom = new JSDOM(balik1)
@@ -137,4 +138,7 @@ news.forEach(a=>silAr.push(a.textContent))
        data.push(`${silAr[5]}`);
 	   
     return data.join(",\n")
+} else {
+return "Bir burç girmelisin veya girilen burç burçlar arasında yok."	
+}
 }
